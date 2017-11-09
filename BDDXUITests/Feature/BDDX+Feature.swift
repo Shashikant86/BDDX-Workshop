28d3b99dbf8b19760e6a6bee90f0594964125c5c
Shashikant86 <shashikant.jagtap@icloud.com>
2017-11-09 15:41:05 +0000

    BDDX Workshop

---
+++ BDDXUITests/Feature/BDDX+Feature.swift
@@ -0,0 +1,15 @@
+//
+//  BDDX+Feature.swift
+//  BDDXUITests
+//
+//  Created by Shashikant Jagtap on 09/11/2017.
+//  Copyright © 2017 Shashikant Jagtap. All rights reserved.
+//
+
+import Foundation
+
+protocol BDDX {
+    
+    func testAppHasEnterButton()
+    func testAppShouldWelcomeUser()
+}
